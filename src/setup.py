f1dbedcfba72a3c431ee365cfff55de2f3bc912d
Hedde van der Heide <me@heddevanderheide.nl>
2022-01-21 12:44:47 +0100

    added MANIFEST.in to include templates

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name='netbox-slm',
-    version='0.9',
-    description='Software Lifecycle Management Netbox Plugin',
-    url='https://github.com/ICTU/netbox_slm',
-    author='Hedde van der Heide',
-    license='Apache 2.0',
-    install_requires=[],
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-)
+setup()
