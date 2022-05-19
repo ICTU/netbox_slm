a53486ca236328bc27eea8a78e84b51ea4c34849
Hedde van der Heide <me@heddevanderheide.nl>
2022-05-19 11:08:57 +0200

    bump version to 1.2

--- netbox_slm/__init__.py
+++ netbox_slm/__init__.py
@@ -5,7 +5,7 @@ class SLMConfig(PluginConfig):
     name = 'netbox_slm'
     verbose_name = 'Software Lifecycle Management'
     description = 'Software Lifecycle Management'
-    version = '1.1'
+    version = '1.2'
     author = 'Hedde van der Heide'
     author_email = 'hedde.vanderheide@ictu.nl'
     base_url = 'slm'
